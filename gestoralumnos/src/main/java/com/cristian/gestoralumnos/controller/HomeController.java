c20df6848f032ef14e453a112779da382df760f5
Brizalas <167039029+Brizalas@users.noreply.github.com>
2026-07-10 08:12:18 +0200

    función delete

--- gestoralumnos/src/main/java/com/cristian/gestoralumnos/controller/HomeController.java
+++ gestoralumnos/src/main/java/com/cristian/gestoralumnos/controller/HomeController.java
@@ -9,52 +9,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 
-
 public class HomeController {
+
+    private List<Alumno> alumnos = new ArrayList<>();
+    private long siguienteId = 4;
+
+    public HomeController() {
+        alumnos.add(new Alumno(1, "laia", "Martinez", "12", null, "Guitarra", "Cristian"));
+        alumnos.add(new Alumno(2, "Miguel", "Garcia", "21", null, "Teatre", "Blanca"));
+        alumnos.add(new Alumno(3, "Laura", "Perez", "42", null, "Dansa", "Olga"));
+    }
+
+    @GetMapping("/")
+    public String mostrarInicio(Model model) {
+        model.addAttribute("titolPagina", "Gestor d'alumnes");
+        model.addAttribute("missatge", "Benvinguda a la meva app de gestió d'alumnes");
+        model.addAttribute("totalAlumnes", alumnos.size());
+        model.addAttribute("alumnes", alumnos);
+
+        return "index";
+    }
+
+    @PostMapping("/afegir-alumne")
+    public String afegirAlumne(
+            @RequestParam String nombre, 
+            @RequestParam String apellido, 
+            @RequestParam String edad, 
+            @RequestParam String modalidad,
+            @RequestParam String profesor
+    ) {
+
+        long nouId = siguienteId; //también se puede hacer 'long nouId = siguienteId ++;'
+        siguienteId ++; //jugadón échale un vistazo, busca donde empieza siguienteId.
+
+        Alumno nouAlumne = new Alumno(
+                nouId,
+                nombre,
+                apellido,
+                edad,
+                null,
+                modalidad,
+                profesor
+        );
+
+        alumnos.add(nouAlumne);
+
+        return "redirect:/";
+
+    }
     
-        private List<Alumno> alumnos = new ArrayList<>();
+    @PostMapping("/eliminar-alumne")
+    public String eliminarAlumne(@RequestParam long id){
+        alumnos.removeIf(alumne -> alumne.getId()== id);
         
-        public HomeController(){
-            alumnos.add(new Alumno(1,"laia","Martinez","12", null, "Guitarra", "Cristian"));
-            alumnos.add(new Alumno(2,"Miguel","Garcia","21",null,"Teatre", "Blanca"));
-            alumnos.add(new Alumno(3,"Laura","Perez","42",null,"Dansa", "Olga"));
-        }
-        
-        @GetMapping("/")
-        public String mostrarInicio(Model model){
-            model.addAttribute("titolPagina","Gestor d'alumnes");
-            model.addAttribute("missatge", "Benvinguda a la meva app de gestió d'alumnes");
-            model.addAttribute("totalAlumnes", alumnos.size());
-            model.addAttribute("alumnes",alumnos);
-            
-            return "index";
-        }
-        
-        @PostMapping("/afegir-alumne")
-        public String afegirAlumne(@RequestParam String nombre){
-            
-            Long nouId = (long) alumnos.size() +1; //id auto incremental, nice!
-            
-            Alumno nouAlumne = new Alumno(
-                    nouId,
-                    nombre,
-                    "",
-                    "",
-                    null,
-                    "",
-                    ""
-                    
-                    );
-            
-            alumnos.add(nouAlumne);
-            
-            return "redirect:/";
-            
-        }
-        
-        
-    
+        return "redirect:/";
+    }
+
 }
